f1c406c0e40ab082ab7d85f2f020911c8f079720
pengzhe <pengzhe@rayvision.com>
2017-11-17 22:37:33 +0800

    add

--- src/main/java/com/javaman/zookeeper/CuratorClient.java
+++ src/main/java/com/javaman/zookeeper/CuratorClient.java
@@ -1,10 +1,24 @@
 package com.javaman.zookeeper;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
 /**
  * Author:彭哲
  * Date:2017/11/17
  */
 public class CuratorClient {
 
-    
+    public static void main(String[] args) throws InterruptedException {
+
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000,3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("119.23.236.253:2181", 5000, 3000, retryPolicy);
+        client.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+    }
+
 }
